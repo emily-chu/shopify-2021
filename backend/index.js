af07157afee6295eb4ddcc466129784675f257e1
emily-chu <emily.megan.chu@gmail.com>
2021-01-16 12:23:35 -0800

    DB functions almost done - "bank" user is loaded with all available images - image purchase works, but should probably be shifted away from using _id's

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,6 @@ const uri = process.env.MONGO_URI;
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
 // Can't test using top-level async (yet): https://stackoverflow.com/questions/46515764/how-can-i-use-async-await-at-the-top-level
 (async function main(){
   try {
@@ -18,8 +17,10 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
     console.log('Connection to shopify_backend did not throw errors');
     //test here
 
-    let test = await server.login(db, 'emily', 'hunter3')
-    console.log(test);
+    // let test = await server.login(db, 'emily', 'hunter2')
+    // console.log(test);
+    // server.loadBank(db); 
+    server.purchase(db, '60033be117ec3383237a299a', '5ff7636667380a5942ec2f9d', '60033b339b34b5bd841276e3');
 
   } catch (err) {
     console.dir(err);
@@ -29,9 +30,12 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'frontend-from-heroku/public')));
 app.set('views', path.join(__dirname, 'frontend-from-heroku'));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => res.render('pages/empty'))
+app.get('/', (req, res) => res.render('pages/empty'));
+app.get('/purchase/:image', (req, res) => {
+  res.render('pages/login')
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const login = async function(db, user, pass){
     const result = await db.collection('users').findOne({username: user, password: pass});
     if (!result) {return null;}
     return {
-      id: result._id,
+      _id: result._id,
       username: result.username,
       money: result.money,
       images: result.images
@@ -17,24 +17,90 @@ const login = async function(db, user, pass){
   } catch (err) {
     console.dir(err);
   }
-}
+};
 
 /**
  * @param {*} connection to shopify_backend
- * @param {String} _id of seller
+ * @param {String} _id of seller (must be the actual owner of the image)
  * @param {String} _id of buyer
  * @param {String} _id of image
  */
+// https://docs.mongodb.com/drivers/node/fundamentals/crud/write-operations/change-a-document
 const purchase = async function(db, sellerId, buyerId, imageId){
   try {
-    const result = await db.collection('users').findOne({username: user, password: pass});
+    if (buyerId == sellerId){
+      console.log('You already own this image.');
+      return;
+    }
+
+    const image = await db.collection('images').findOne({_id: ObjectId(imageId)});
+    if (!image){
+      console.log('Invalid image.');
+      return;
+    }
+
+    const buyer = await db.collection('users').findOne({_id: ObjectId(buyerId)});
+    if (buyer.money < image.cost){
+      console.log('You don\'t have enough money for this image! Try selling some of your images if you want this one.');
+      return;
+    }
+
+    const sellerUpdate = {
+      // $inc: {money: image.cost},
+      $pull: {images: ObjectId(imageId)}
+    };
+    const buyerUpdate = {
+      // $inc: {money: -image.cost},
+      $addToSet: {images: ObjectId(imageId)}
+    };
+    await db.collection('users').updateOne({_id: ObjectId(sellerId)}, sellerUpdate, function(err, res){});
+    await db.collection('users').updateOne({_id: ObjectId(buyerId)}, buyerUpdate, function(err, res){});
   } catch (err) {
     console.dir(err);
+  }
+};
+
+const axios = require ('axios');
+const { ObjectId } = require('mongodb');
+/**
+ * Initializes database with every photo picsum.photos has to offer; then adds these images to the Bank
+ * @param {*} connection to shopify_backend
+ */
+const loadBank = async function(db) {
+  let documents = [];
+
+  // Picsum API returns a maximum of 100 images per page, and only has 11 pages (0-10)
+  for (let page = 0; page <= 10; page++){ 
+    try {
+      let response = await axios.get('https://picsum.photos/v2/list?page=' + page.toString() + '&limit=100')
+      let pageDocuments = response.data;
+      for (let img of pageDocuments){
+        img.cost = Math.round(Math.random() * 100 * (page + 1)/2);
+      }
+      console.log(pageDocuments)
+      documents.push(...pageDocuments);
+    } catch (err) {
+      console.dir(err);
+      return {message: 'Axios couldn\'t retrieve the image list. Page: ' + page.toString()};
+    }
+  }
+  
+  try {
+    await db.collection('images').insertMany(documents);
+    db.collection('images').distinct('_id', {}, {}, function(err, result) {
+      db.collection('users').updateOne(
+        {username: 'BANK', password: undefined}, 
+        {$set: {images: result}}
+      );
+    });
+  } catch (err) {
+    return {message: 'Loading images to DB failed.'};
   }
 }
 
 // public functions
 module.exports = {
+  loadBank: loadBank,
   login: login,
-  purchase: purchase
+  purchase: purchase,
 };
