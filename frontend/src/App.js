64fe7517f5cc300b31488244b1fdd634e704b0e1
emily-chu <emily.megan.chu@gmail.com>
2021-01-24 10:29:40 -0800

    Frontend: App skeleton from create-react-app - Design is sort of there - wow so *neumorphic*

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+
+function App() {
+  return (
+
+<div className="app">
+  <div className="search-bar popin">
+    eee
+  </div>
+  <div className="main-row">
+    <div className="results-panel popout">
+      eee
+    </div>
+    <div className="nominations-panel popout">
+      eee
+    </div>
+  </div>
+</div>
+
+  );
+}
+
+export default App;
